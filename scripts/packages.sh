b7fadf9009f2ad434c195dd47875e89e45398549
Kender-Mendoza <kendermendoza591@outlook.com>
2021-08-15 16:31:00 -0600

    Se mejoro la configuracin de vim

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -1,5 +1,5 @@
 # !/bin/bash
 # Paquetes necesarios
-sudo su -c "apt install zsh curl -y"
+sudo su -c "apt install zsh curl vim -y"
 # Oh my zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
